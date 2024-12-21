7f6ef44b07d9d8824b469979d7c1bf001096aa6e
Jackie Zhao <zjackie226@gmail.com>
2024-12-21 13:47:04 -0500

    enabled parallel testing, works with 8 workers on local machine, testing with 8 workers in CICD. Fixed an issue where test 6 will fail in headless mode

--- POM/ContactUsPOM.ts
+++ POM/ContactUsPOM.ts
@@ -8,6 +8,12 @@ export class ContactUsPOM{
     }
 
     async contact(name:string, email:string, subject:string, message:string, filePath:string){
+        //note down how to handle dialog popups
+        this.page.on('dialog', async(dialog)=>{
+            console.log(`Dialog Message: ${dialog.message()}`);
+            await dialog.accept();
+        });
+
         await this.page.locator("//input[@name='name' and @data-qa='name']").fill(name);
         await this.page.locator("//input[@name='email' and @data-qa='email']").fill(email);
         await this.page.locator("//input[@name='subject' and @data-qa='subject']").fill(subject);
@@ -15,12 +21,9 @@ export class ContactUsPOM{
         //note how to upload files
         await this.page.locator("//input[@type='file' and @name='upload_file']").setInputFiles(filePath);
 
-        //note down how to handle dialog popups
-        this.page.on('dialog', async(dialog)=>{
-            console.log(`Dialog Message: ${dialog.message()}`);
-            await dialog.accept();
-        })
-
+        // Wait 1ms for some reason in headless mode it requires a delay or else the test will fail
+        // I believe this is due to the dialog popup behavior, in headed mode there is no need for delay, in headless it will fail without delay
+        await this.page.waitForTimeout(100);
         await this.page.getByRole('button',{name:'Submit'}).click();
     }
 }
